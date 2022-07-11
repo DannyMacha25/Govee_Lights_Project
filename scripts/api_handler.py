c0bb727664deea34639aaef79cfd5574b796cd3d
DannyMacha25 <91552897+DannyMacha25@users.noreply.github.com>
2022-07-11 15:10:16 -0400

    Lights turn on!

--- scripts/api_handler.py
+++ scripts/api_handler.py
@@ -1,17 +1,25 @@
 import requests
 import json
+import io
 
-URL = 'https://developer-api.govee.com/v1/devices'
-HEADERS = {'Govee-API-Key' : '6a43de81-80b9-4a68-8fb6-3fedead8421c'}
+f = open('api_key.txt','r')
+API_KEY = f.read()
+
+f.close()
+
+
+URL = 'https://developer-api.govee.com/v1/devices/control'
+HEADERS = {'Govee-API-Key' : API_KEY, 'content-type': 'application/json'}
 DATA = {
-    'device':'BB:D3:A4:C1:38:47:77:C2',
-    'model':'H6110',
-    'cmd':{
-        'name':'turn',
-        'value':'on'
-    }
+    'device': 'BB:D3:A4:C1:38:47:77:C2',
+    'model': 'H6110',
+    'cmd': {
+	'name': 'turn',
+	'value': 'on'
 }
-r = requests.put(URL,headers=HEADERS,data=DATA)
+}
+r = requests.put(URL,headers=HEADERS,data=json.dumps(DATA))
 
 #data = r.json()
-#print(r.text)
+print(r)
+print(r.content)
